ee5e052a3a9ec305822968b688cdc98f7b4e01ff
Barry digby <b.digby237@gmail.com>
2022-01-20 11:51:19 +0000

    s

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Cloud CIX'
+copyright = '2021, Barry Digby'
+author = 'Barry Digby'
 
 release = '0.1'
 version = '0.1.0'
