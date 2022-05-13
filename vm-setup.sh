e17f6d56cb110c5735c8ad283f7e2bc3aac76497
Barry digby <b.digby237@gmail.com>
2022-05-13 14:41:28 +0100

    auto yes to conda

--- vm-setup.sh
+++ vm-setup.sh
@@ -91,4 +91,5 @@ sudo apt-get -y install libgl1-mesa-glx \
         libxml2-dev
 
 wget https://repo.anaconda.com/archive/Anaconda3-2021.05-Linux-x86_64.sh
-bash Anaconda3-2021.05-Linux-x86_64.sh
+yes | bash Anaconda3-2021.05-Linux-x86_64.sh
+
